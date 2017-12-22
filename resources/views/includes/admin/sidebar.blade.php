2efe82ea34af3a54b109a1a9e5a9dec0dd8754b1
rizkarahayu <rizkarahayu05@gmail.com>
2017-12-22 09:45:54 +0700

    ganti transaksi

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -23,59 +23,35 @@
               </span>
                 </a>
                 <ul class="treeview-menu">
-<<<<<<< HEAD
-                    <li><a href="{{ url('/ck-admin/users') }}"><i class="fa fa-user-circle">  Users</i></a></li>
-                    <li><a href="{{ url('/ck-admin/customer') }}"><i class="fa fa-user-o">  Customer</i></a></li> 
-                    <li><a href="{{ url('/ck-admin/toko') }}"><i class="fa fa-shopping-bag">  Toko </i></a></li>
-                    <li><a href="{{ url('/ck-admin/produk') }}"><i class="fa fa-birthday-cake">  Produk</i></a></li>
-                    <li><a href="{{ url('/ck-admin/events') }}"><i class="fa fa-link">  Events </i></a></li>
-=======
                     <li><a href="{{ url('/ck-admin/users') }}"><i class="fa fa-user-circle"></i>Users</a></li>
                     <li><a href="{{ url('/ck-admin/customer') }}"><i class="fa fa-user-plus"></i>Customers</a></li>
                     <li><a href="{{ url('/ck-admin/toko') }}"><i class="fa fa-institution"></i>Toko</a></li>
                     <li><a href="{{ url('/ck-admin/users') }}"><i class="fa fa-shopping-bag"></i>Produk</a></li>
                     <li><a href="{{ url('/ck-admin/events') }}"><i class="fa fa-gift"></i>Events</a></li>
->>>>>>> 2ad3978050d04ce3154a8ecdfc69da0dc491ced8
-                    
-
                 </ul>
             </li>
              <li class="treeview">
-<<<<<<< HEAD
-                <a href="#"><i class="fa fa-cc-mastercard"></i> <span>Data Transaksi</span>
-=======
                 <a href="#"><i class="fa fa-handshake-o"></i> <span>Data Transaksi</span>
->>>>>>> 2ad3978050d04ce3154a8ecdfc69da0dc491ced8
                     <span class="pull-right-container">
                 <i class="fa fa-angle-left pull-right"></i>
               </span>
                 </a>
                 <ul class="treeview-menu">
-<<<<<<< HEAD
-                    <li><a href="{{ url('/ck-admin/transaksi') }}"><i class="fa fa-cc-paypal">  Transaksi</i></a></li>
-                    <li><a href="{{ url('/ck-admin/detailtransaksi') }}"><i class="fa fa-credit-card-alt">  Detail Transaksi</i></a></li> 
-                </ul>
-            </li>
-            <li class="treeview">
-                <a href="#"><span><i class="fa fa-line-chart"> Laporan</i></span>
-=======
                     <li><a href="{{ url('/ck-admin/transaksi') }}"><i class="fa fa-history"></i>Transaksi</a></li>
                     <li><a href="{{ url('/ck-admin/detailtransaksi') }}"><i class="fa fa-eye"></i>Detail Transaksi</a></li> 
                 </ul>
             </li>
             <li class="treeview">
                 <a href="#"><i class="fa fa-newspaper-o"></i> <span>Laporan</span>
->>>>>>> 2ad3978050d04ce3154a8ecdfc69da0dc491ced8
+
                     <span class="pull-right-container">
                 <i class="fa fa-angle-left pull-right"></i>
               </span>
                 </a>
                 <ul class="treeview-menu">
-<<<<<<< HEAD
-                    <li><a href="{{ url('/ck-admin/laporanbln') }}"><i class="fa fa-bar-chart-o"> Laporan Perbulan</i></a></li>
-=======
+
                     <li><a href="{{ url('/ck-admin/laporanbln') }}"><i class="fa fa-area-chart"></i>Laporan Per Bulan</a></li>
->>>>>>> 2ad3978050d04ce3154a8ecdfc69da0dc491ced8
+
                 </ul>
             </li>
         </ul>
