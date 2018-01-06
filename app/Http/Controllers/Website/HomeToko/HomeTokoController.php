dc57882632cca5a24df3dc70603d3aa2493e6c75
rezaprillia <rezaprillia25@gmail.com>
2018-01-06 14:57:18 +0700

    halaman toko

--- app/Http/Controllers/Website/HomeToko/HomeTokoController.php
+++ app/Http/Controllers/Website/HomeToko/HomeTokoController.php
@@ -11,11 +11,12 @@ class HomeTokoController extends Controller
          $toko  = app('toko')->get();
          $list = [];
          $list['id_produk'] = '10';
+         $list['nama_toko'] = 'Banana Foster Lampung';
          $list['nama_produk'] = 'Caramelized Foster Lampung';
          $list['deskripsi_produk'] = 'Perpaduan cake lembut dengan coklat dan karamel yang enak. Karamel super yummy lumer di mulut. Perfect mix!';
          $list['harga'] = 'Rp68.000,00';
          $list['harga_diskon'] = 'Rp65.000,00'; 
          $list['review'] = "Pengiriman sangat cepat";
-         return view('websites.hometoko.hometoko', compact('list','toko'));
+         return view('websites.hometoko.hometoko', compact('list'));
     }
 }
